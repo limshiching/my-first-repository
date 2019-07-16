389e0ed5c018d078de5c03038ce4fba3b26d7202
Lim Shi Ching <limshiching@Lims-MacBook-Pro.local>
2019-07-16 15:25:15 +0800

    added sign in and sign out button

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/<name>")
-def index(name):
-    name = name.upper()
-    return render_template('index.html', name=name)
+@app.route("/")
+def index():
+    signed_in = False
+    return render_template('index.html', signed_in=signed_in)
 
 
 # @app.route("/another")
