ee9a089faf82c13a4c9b27934fd810591b719780
Lim Shi Ching <limshiching@Lims-MacBook-Pro.local>
2019-07-16 14:58:15 +0800

    showed name from url parmeter in template

--- server.py
+++ server.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")  # Revisit decorators if you unclear of this syntax
-def index():
-    return render_template('index.html')
+@app.route("/<name>")
+def index(name):
+    name = name.upper()
+    return render_template('index.html', name=name)
 
 
 # @app.route("/another")
@@ -12,10 +13,10 @@ def index():
 #     return '<h1>Yo</h1>'
 
 
-@app.route('/user/<username>')
-def show(username):
-    return f"Hi {username}"
+# @app.route('/user/<username>')
+# def show(username):
+#     return f"Hi {username}"
 
 
-if __name__ == '__main__':  # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
