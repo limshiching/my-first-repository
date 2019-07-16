f05d09dca6a97fd2b894bba535b7b207a3db74b6
Lim Shi Ching <limshiching@Lims-MacBook-Pro.local>
2019-07-16 13:31:11 +0800

    made show route show username

--- server.py
+++ server.py
@@ -7,9 +7,14 @@ def index():
     return '<h1>Why so easy</h1>'
 
 
-@app.route("/another")
-def show():
-    return '<h1>Yo</h1>'
+# @app.route("/another")
+# def show():
+#     return '<h1>Yo</h1>'
+
+
+@app.route('/user/<username>')
+def show(username):
+    return f"Hi {username[0:5]}"
 
 
 if __name__ == '__main__':  # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
