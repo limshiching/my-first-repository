9af6f651a968c032f39db5ab1fe4996565e74bc8
Lim Shi Ching <limshiching@Lims-MacBook-Pro.local>
2019-07-16 14:18:09 +0800

    added templates

--- server.py
+++ server.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")  # Revisit decorators if you unclear of this syntax
 def index():
-    return '<h1>Why so easy</h1>'
+    return render_template('index.html')
 
 
-@app.route("/another")
-def show():
-    return '<h1>Yo</h1>'
+# @app.route("/another")
+# def show():
+#     return '<h1>Yo</h1>'
+
+
+@app.route('/user/<username>')
+def show(username):
+    return f"Hi {username}"
 
 
 if __name__ == '__main__':  # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
-    app.run()
+    app.run(debug=True)
